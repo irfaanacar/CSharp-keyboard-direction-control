d83d16c3927695505996dc459f315b7f55ca64ce
Irfan <irfaanacar@gmail.com>
2023-09-02 20:00:34 +0300

    CSharp keyboard direction control
    In this application, I have designed and coded a picture box that can move when any key on the keyboard is pressed.

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace klavyeileyonkontrolu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(578, 237);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(157, 146);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(100, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 618);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(1191, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 618);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(121, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1070, 28);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(121, 549);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(1070, 28);
+            this.label4.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1329, 618);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace klavyeileyonkontrolu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int xkonumu = pictureBox1.Location.X;
+            int ykonumu = pictureBox1.Location.Y;
+            if (e.KeyCode == Keys.W && ykonumu > 0)
+            {
+                ykonumu = ykonumu - 8;//-5 olmasının sebebi nesne yukarıya çıktıkça konumunun değeri azalır bunun için en yukarda 0'dır.
+            }
+            if  (e.KeyCode == Keys.S && ykonumu<380)
+            {
+                ykonumu = ykonumu + 8;
+            }
+            if (e.KeyCode == Keys.A && xkonumu>0)
+            {
+                xkonumu = xkonumu - 8; //kaç piksellik hareket sağlayacağını belirleyebiliriz. Ben 8 yaptım.
+            }
+            if (e.KeyCode == Keys.D && xkonumu < 880)
+            {
+                xkonumu= xkonumu + 8;
+            }
+            pictureBox1.Location = new Point(xkonumu, ykonumu);
+            if (pictureBox1.Left <= label1.Left)
+            {
+                label1.BackColor = Color.Crimson;
+            }
+            if (pictureBox1.Right >= label2.Left)
+            {
+                label2.BackColor = Color.Yellow;
+            }
+            if(pictureBox1.Top <= label3.Bottom)
+            {
+                label3.BackColor = Color.Blue;
+            }
+            if (pictureBox1.Bottom >= label4.Top)
+            {
+                label4.BackColor = Color.Green;
+            }
+    }
+  }
+}
+
